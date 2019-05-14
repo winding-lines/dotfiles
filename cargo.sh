e4b9b7a880e52679efa5d1197c48a25dd677b9ef
winding-lines <git@winding-lines.com>
2019-05-14 06:51:41 -0700

    cargo.sh force install binaries

--- cargo.sh
+++ cargo.sh
@@ -1,7 +1,7 @@
 command -v cargo 2> /dev/null 1>&2
 if [ $? -eq 0  ] ; then
-  cargo install ripgrep
-  cargo install bat
-  cargo install lsd
+  for cmd in ripgrep bat lsd ; do
+    cargo install -f $cmd
+  done
 fi
 
