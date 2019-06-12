e02f9126ada22a51fd23f296ea70f70a4f4bbe6d
winding-lines <git@winding-lines.com>
2019-06-12 04:33:51 -0700

    Use shorter directory name in prompt

--- bash/prompt.sh
+++ bash/prompt.sh
@@ -2,6 +2,6 @@ F=`brew --prefix git`/etc/bash_completion.d/git-prompt.sh
 if [ -f $F ] ; then
   source $F
   GIT_PS1_SHOWDIRTYSTATE=true
-  export PS1='\h: \w $(__git_ps1)\$ '
+  export PS1='\h: \W $(__git_ps1)\$ '
 fi
 
