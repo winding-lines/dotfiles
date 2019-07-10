91de736145a272b88b9ff282221f95ed65c93675
winding-lines <git@winding-lines.com>
2019-07-10 09:39:47 -0700

    Fix git prompt on linux

--- bash/prompt.sh
+++ bash/prompt.sh
@@ -1,4 +1,9 @@
-F=`brew --prefix git`/etc/bash_completion.d/git-prompt.sh
+command -c brew > /dev/null 2>&1
+if [ $? -eq 0 ] ; then
+  F=`brew --prefix git`/etc/bash_completion.d/git-prompt.sh
+else
+  F=$HOME/dotfiles/git-prompt.sh
+fi
 if [ -f $F ] ; then
   source $F
   GIT_PS1_SHOWDIRTYSTATE=true
