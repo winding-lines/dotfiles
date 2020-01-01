c954b354ad913e4477e026b2c7cad2235b5a5c4e
Marius Seritan <git@winding-lines.com>
2020-01-01 06:30:39 -0800

    Install Fira Code font using brew

--- macos.sh
+++ macos.sh
@@ -1,4 +1,5 @@
-if [ ! -f $HOME/Library/Fonts/FiraCode-Regular.ttf ] ; then
-  echo Install Fira font from https://github.com/tonsky/FiraCode/wiki
-  exit 1
+if [ ! -f $HOME/Library/Fonts/FiraCode-Regular.otf ] ; then
+  echo Installing Fira font from https://github.com/tonsky/FiraCode/wiki
+  brew tap homebrew/cask-fonts
+  brew cask install font-fira-code
 fi
